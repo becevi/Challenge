bc724a5f2c086cd93f326cac1ac70e7f14c93850
becevi <benjaminceavidal@gmail.com>
2021-08-04 14:19:46 -0500

    Created and updated Admin model, configure view page with different requirements for Admin sign up, set up Admin login and log out button

--- db/migrate/20210804051523_devise_create_admins.rb
+++ db/migrate/20210804051523_devise_create_admins.rb
@@ -9,6 +9,7 @@ class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.string :phone
+      
       t.decimal :earnings
       ## Recoverable
       t.string   :reset_password_token
