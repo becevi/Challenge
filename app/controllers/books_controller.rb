bc5ab7baf7f6a56efc87d5f722751d8a61a0bc3c
becevi <benjaminceavidal@gmail.com>
2021-08-04 18:03:06 -0500

    Stablished new routes, added new and create actions in books controller, added books params, created admins dashboard, added form for admin to create new book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,24 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
+    def new
+        @book = Book.new
+    end
+
+    
+    def create 
+        @book = Book.new(book_params) 
+        if @book.save 
+            redirect_to(admins_authenticated_root_path) and return
+        else               
+            render '/books/new' 
+        end 
+    end 
+    
+    private 
+    def book_params 
+      params.require(:book).permit(:title, :first_name, :last_name, :description, :price, :stock) 
+    end 
+
 end
