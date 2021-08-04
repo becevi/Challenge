97a0fcd6e3cfcee336cde223845f527a9d20a333
becevi <benjaminceavidal@gmail.com>
2021-08-04 18:40:40 -0500

    Created books#new page where admin can offer new books, added link in admin dashboard, declared after_sign_up action for user and admin

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,4 +3,9 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_resource, except: [:new, :create]
+
+  def after_sign_up_path_for(resource)
+    admins_authenticated_root_path 
+  end
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include Accessible
   skip_before_action :check_resource, except: [:new, :create]
+
+  def after_sign_up_path_for(resource)
+    users_authenticated_root_path 
+  end
+
 end
