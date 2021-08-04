a935086d98b47a98eb7d0beb8c2bf77d437118c9
becevi <benjaminceavidal@gmail.com>
2021-08-04 01:18:51 -0500

    Implemented Devise

--- db/migrate/20210804042604_create_seller_book.rb
+++ db/migrate/20210804042604_create_seller_book.rb
@@ -1,8 +1,9 @@
-class CreateSellerBooks < ActiveRecord::Migration[6.1]
+class CreateSellerBook < ActiveRecord::Migration[6.1]
   def change
     create_table :seller_books, id: false do |t|
       t.belongs_to :seller
       t.belongs_to :book
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_03_210553) do
+ActiveRecord::Schema.define(version: 2021_08_04_051523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -68,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_08_03_210553) do
   create_table "seller_books", id: false, force: :cascade do |t|
     t.bigint "seller_id"
     t.bigint "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_seller_books_on_book_id"
     t.index ["seller_id"], name: "index_seller_books_on_seller_id"
   end
@@ -81,6 +95,18 @@ ActiveRecord::Schema.define(version: 2021_08_03_210553) do
     t.decimal "earnings"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "carts", "buyers"
