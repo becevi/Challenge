40a346339aed83c6f944610559ed999eae74a519
becevi <benjaminceavidal@gmail.com>
2021-08-04 21:15:39 -0500

    Fixed bug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+    #index action to check if some substring of the title is present and if no query
+    # done then just show all books
     def index
         if params[:query].present?
           @books = Book.where("title ILIKE ?", "%#{params[:query]}%")
@@ -8,11 +10,13 @@ class BooksController < ApplicationController
         end
     end
 
+    # this allows the get request for the book form
     def new
         @book = Book.new
     end
 
-    
+    # creates a new instance of book and if saved goes back to admin dashboard
+    #else it asks goes back to the form
     def create 
         @book = Book.new(book_params) 
         if @book.save 
@@ -22,6 +26,7 @@ class BooksController < ApplicationController
         end 
     end 
     
+    #params to retrieve safely the book params
     private 
     def book_params 
       params.require(:book).permit(:title, :first_name, :last_name, :description, :price, :stock) 
